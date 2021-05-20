5ae939936269efed0ed6d14199754d1dc6e31e92
laiaBT <84039069+laiaBT@users.noreply.github.com>
2021-05-20 16:38:49 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,77 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . 8 . . . . . . 
+        . . . . . . . . . 8 . . . . . . 
+        . . . . . . . . 7 8 . . . . . . 
+        . . . . . . . . 7 8 . . . . . . 
+        . . . . . . . . 7 8 . . . . . . 
+        . . . . . . . . 7 . . . . . . . 
+        . . . . . . 7 . 7 8 . . . . . . 
+        . . . . . . 7 . 7 8 . . . . . . 
+        . . . . . . 7 . 7 7 . . . . . . 
+        . . . . . 8 7 7 8 7 . . . . . . 
+        . . . . . 8 7 8 8 7 . . . . . . 
+        . . . . . 8 8 8 8 7 . . . . . . 
+        . . . . . . 8 7 7 7 . . . . . . 
+        . . . . . . 8 8 7 7 . . . . . . 
+        `, mySprite, 0, -50)
+    music.pewPew.play()
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    mySprite.destroy()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 500)
+    scene.cameraShake(4, 500)
+    info.changeLifeBy(-1)
+})
+let ASTEROIDE: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 6 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 6 . . . . . . . 
+    . . . . . . 8 8 9 8 . . . . . . 
+    . . . . . . 8 6 9 8 . . . . . . 
+    . . . . . c c c 8 8 8 . . . . . 
+    . . . . 8 8 6 6 6 9 8 8 . . . . 
+    . . 8 f f f c c e e f f 8 8 . . 
+    . 8 8 8 8 8 8 6 6 6 6 9 6 8 8 . 
+    8 8 8 8 8 8 8 8 6 6 6 9 6 6 8 8 
+    8 8 8 8 8 8 8 8 6 6 6 6 9 6 8 8 
+    `, SpriteKind.Player)
+mySprite.setPosition(77, 32)
+controller.moveSprite(mySprite, 100, 100)
+mySprite.setStayInScreen(true)
+game.onUpdateInterval(1000, function () {
+    ASTEROIDE = sprites.createProjectileFromSide(img`
+        . . . . . . . . . c c 8 . . . . 
+        . . . . . . 8 c c c f 8 c c . . 
+        . . . c c 8 8 f c a f f f c c . 
+        . . c c c f f f c a a f f c c c 
+        8 c c c f f f f c c a a c 8 c c 
+        c c c b f f f 8 a c c a a a c c 
+        c a a b b 8 a b c c c c c c c c 
+        a f c a a b b a c c c c c f f c 
+        a 8 f c a a c c a c a c f f f c 
+        c a 8 a a c c c c a a f f f 8 a 
+        . a c a a c f f a a b 8 f f c a 
+        . . c c b a f f f a b b c c 6 c 
+        . . . c b b a f f 6 6 a b 6 c . 
+        . . . c c b b b 6 6 a c c c c . 
+        . . . . c c a b b c c c . . . . 
+        . . . . . c c c c c c . . . . . 
+        `, 0, 50)
+    ASTEROIDE.x += randint(0, scene.screenWidth())
+    ASTEROIDE.setKind(SpriteKind.Enemy)
+})
